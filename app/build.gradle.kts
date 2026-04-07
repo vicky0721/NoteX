35cf51623c7aa8ee3246711237208a57b51111b8
Vikram Sahni <kumarvikram9356@gmail.com>
2026-04-08 00:33:09 +0530

    Fix dependencies and added JitPack repository

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    kotlin("kapt")
 }
 
 android {
@@ -37,6 +38,16 @@ android {
 
 dependencies {
 
+
+    val lifecycle_version = "2.6.1"
+    val room_version = "2.5.1"
+    val navigation_version = "2.5.3"
+
+//    implementation("android.legacy:legacy-support-v4:1.0.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycle_version")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -45,4 +56,52 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("androidx.recyclerview:recyclerview:1.3.0")
+    implementation("androidx.room:room-runtime:$room_version")
+
+    //Recycler View animator
+    implementation("jp.wasabeef:recyclerview-animators:4.0.2")
+
+    //View Model
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycle_version")
+    //LiveData
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
+    //Annotation processor
+    implementation("androidx.lifecycle:lifecycle-common-java8:2.10.0")
+
+    //Room
+    //implementation("androidx.room:room-runtime:$room_version")
+    kapt("androidx.room:room-compiler:$room_version")
+
+    //kotlin extension and coroutines support for Room
+    implementation("androidx.room:room-ktx:$room_version")
+
+    //coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.3.9")
+
+    //navigation process
+    implementation("androidx.fragment:fragment-ktx:1.5.5")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.9.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.9.7")
+
+    //material components
+    implementation("com.google.android.material:material:1.9.0")
+
+    //color picker library
+    implementation("com.github.skydoves:colorpickerview:2.2.4")
+   // implementation("com.thebluealliance:spectrum-picker:2.1.1")
+
+    //implementation("com.yahiaangleo.markdownedittext:markdownedittext:1.0.0")
+    implementation("io.noties.markwon:core:4.6.2")
+    implementation("io.noties.markwon:ext-strikethrough:4.6.2")
+    implementation("io.noties.markwon:ext-tasklist:4.6.2")
+    implementation("io.noties.markwon:ext-tables:4.6.2")
+
+
+
+
 }
